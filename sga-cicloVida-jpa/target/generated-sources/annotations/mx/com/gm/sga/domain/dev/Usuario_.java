1baf4eae66c9e4c8597565d9794623ba5ca29937
jamhir789 <erick.medina@itspanuco.edu.mx>
2018-10-24 10:47:11 -0500

    initial commmit

---
+++ sga-cicloVida-jpa/target/generated-sources/annotations/mx/com/gm/sga/domain/dev/Usuario_.java
@@ -0,0 +1,18 @@
+package mx.com.gm.sga.domain.dev;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+import mx.com.gm.sga.domain.Persona;
+import mx.com.gm.sga.domain.Usuario;
+
+@Generated(value="EclipseLink-2.6.1.v20150605-rNA", date="2018-10-24T10:33:21")
+@StaticMetamodel(Usuario.class)
+public class Usuario_ { 
+
+    public static volatile SingularAttribute<Usuario, String> password;
+    public static volatile SingularAttribute<Usuario, Persona> persona;
+    public static volatile SingularAttribute<Usuario, Integer> idUsuario;
+    public static volatile SingularAttribute<Usuario, String> username;
+
+}
---
+++ sga-cicloVida-jpa/target/generated-sources/annotations/mx/com/gm/sga/domain/prod/Persona_.java
@@ -0,0 +1,19 @@
+package mx.com.gm.sga.domain.prod;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+import mx.com.gm.sga.domain.Persona;
+
+@Generated(value="EclipseLink-2.6.1.v20150605-rNA", date="2018-10-24T10:33:21")
+@StaticMetamodel(Persona.class)
+public class Persona_ { 
+
+    public static volatile SingularAttribute<Persona, String> apeMaterno;
+    public static volatile SingularAttribute<Persona, String> telefono;
+    public static volatile SingularAttribute<Persona, String> nombre;
+    public static volatile SingularAttribute<Persona, String> apePaterno;
+    public static volatile SingularAttribute<Persona, Integer> idPersona;
+    public static volatile SingularAttribute<Persona, String> email;
+
+}
